308075fa1628b6fccd8aaf4a552a0ced3b99111c
Simon Stack <stacula@gmail.com>
2020-08-31 10:03:02 +0800

    ineb past

---
+++ inebriated-past/scripts.js
@@ -0,0 +1,89 @@
+function writeHeadTags()  {
+
+    document.write("<title>Proust Outline and Annotations</title>");
+    document.write("<meta http-equiv='content-type' content='text/html;charset=utf-8'>");
+    document.write("<meta http-equiv='Content-Style-Type' content='text/css'>");
+    document.write("<link rel='stylesheet' type='text/css' href='/global.css' />");
+
+}
+
+
+
+function writeHeader() {
+
+    document.write("<div id='page-header'><img src='/proust.jpg'>&nbsp;&nbsp;<span id='page-title'>Proust Outline and Annotations</span></div>");
+    document.write("<div id='cssmenu'>");
+    document.write("<ul>");
+    document.write("<li><a href='/index.html'>Home</a></li>");
+    document.write("<li class='has-sub'><a href='/#'>About</a>");
+    document.write("<ul>");
+    document.write("<li><a href='/background.html'>Background</a></li>");
+    document.write("<li><a href='/how-to.html'>How to Use This Site</a></li>");
+    document.write("<li><a href='/about-the-text.html'>About The Text Used Here</a></li>");
+    document.write("</ul>");
+    document.write("</li>");
+    document.write("<li><a href='/outline.html'>Outline</a></li>");
+    document.write("<li class='has-sub'><a href='#'>The Text</a>");
+    document.write("<ul>");
+    document.write("<li class='has-sub'><a href='#'>Swann's Way</a>");
+    document.write("<ul>");
+    document.write("<li><a href='/01-swann-overture.html'>Overture</a></li>");
+    document.write("<li><a href='/02-swann-combray.html'>Combray</a></li>");
+    document.write("<li><a href='/03-swann-swann-in-love.html'>Swann In Love</a></li>");
+    document.write("<li><a href='/04-swann-place-names-the-name.html'>Place-Names: The Name</a></li>");
+    document.write("</ul>");
+    document.write("</li>");
+    document.write("<li class='has-sub'><a href='#'>Within a Budding Grove</a>");
+    document.write("<ul>");
+    document.write("<li><a href='/05-budding-mme-swann-at-home.html'>Mme Swann at Home</a></li>");
+    document.write("<li><a href='/06-budding-place-names-the-place.html'>Place-Names: The Place</a></li>");
+    document.write("<li><a href='/07-budding-seascape-with-frieze-of-girls.html'>Seascape, With Frieze of Girls</a></li>");
+    document.write("</ul>");
+    document.write("</li>");
+    document.write("<li class='has-sub'><a href='#'>The Guermantes Way</a>");
+    document.write("<ul>");
+    document.write("<li><a href='/08-guermantes-chapter-one.html'>Chapter One</a></li>");
+    document.write("<li><a href='/09-guermantes-chapter-two.html'>Chapter Two</a></li>");
+    document.write("</ul>");
+    document.write("</li>");
+    document.write("<li class='has-sub'><a href='#'>Cities of the Plain</a>");
+    document.write("<ul>");
+    document.write("<li><a href='/10-cities-chapter-one.html'>Chapter One</a></li>");
+    document.write("<li><a href='/11-cities-chapter-two.html'>Chapter Two</a></li>");
+    document.write("<li><a href='/12-cities-chapter-three.html'>Chapter Three</a></li>");
+    document.write("<li><a href='/13-cities-chapter-four.html'>Chapter Four</a></li>");
+    document.write("</ul>");
+    document.write("</li>");
+    document.write("<li class='has-sub'><a href='#'>The Captive</a>");
+    document.write("<ul>");
+    document.write("<li><a href='/14-captive-chapter-one.html'>Chapter One</a></li>");
+    document.write("<li><a href='/15-captive-chapter-two.html'>Chapter Two</a></li>");
+    document.write("<li><a href='/16-captive-chapter-three.html'>Chapter Three</a></li>");
+    document.write("</ul>");
+    document.write("</li>");
+    document.write("<li class='has-sub'><a href='#'>The Sweet Cheat Gone</a>");
+    document.write("<ul>");
+    document.write("<li><a href='/17-fugitive-chapter-one.html'>Chapter One</a></li>");
+    document.write("<li><a href='/18-fugitive-chapter-two.html'>Chapter Two</a></li>");
+    document.write("<li><a href='/19-fugitive-chapter-three.html'>Chapter Three</a></li>");
+    document.write("<li><a href='/20-fugitive-chapter-four.html'>Chapter Four</a></li>");
+    document.write("</ul>");
+    document.write("</li>");
+    document.write("<li class='has-sub'><a href='#'>Time Regained</a>");
+    document.write("<ul>");
+    document.write("<li><a href='/21-time-regained-chapter-one.html'>Chapter One</a></li>");
+    document.write("<li><a href='/22-time-regained-chapter-two.html'>Chapter Two</a></li>");
+    document.write("<li><a href='/23-time-regained-chapter-three.html'>Chapter Three</a></li>");
+    document.write("</ul>");
+    document.write("</li>");
+    document.write("<li class='has-sub'><a href='#'>Auxiliary Texts</a>");
+    document.write("<ul>");
+    document.write("<li><a href='/auxiliary/csb-masturbation-scene.html'>Masturbation Passage from Contre Saint-Beauve</a></li>");
+    document.write("</ul>");
+    document.write("</li>");
+    document.write("</ul>");
+    document.write("</li>");
+    document.write("</ul>");
+    document.write("</div>");
+
+}
